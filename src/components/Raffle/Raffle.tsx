da90b19df7d957de5db5121f08ad8e1a99afcc92
George Biryukov <george@biryukov.ws>
2022-10-17 18:53:31 -0400

    add code check

--- src/components/Raffle/Raffle.tsx
+++ src/components/Raffle/Raffle.tsx
@@ -1,3 +1,51 @@
+import { ChangeEvent, useState } from 'react';
+
+const CORRECT_CODE = '38';
+
 export const Raffle = () => {
-  return <>'raffle'</>
-}
+  const [code, setCode] = useState('');
+  const [isCodeCorrect, setIsCodeCorrect] = useState<boolean | null>(null);
+
+  const handleCodeChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setCode(event.currentTarget.value.slice(0, 2));
+  };
+
+  const handleCheck = () => {
+    setIsCodeCorrect(code === CORRECT_CODE);
+  };
+
+  const renderStatus = () => {
+    if (isCodeCorrect === null) {
+      return <div className="raffle__modal-title">Enter code</div>;
+    }
+
+    if (isCodeCorrect) {
+      return <div className="raffle__modal-title">Hooray!</div>;
+    }
+
+    return (
+      <div className="raffle__modal-title raffle__modal-title_error">Na-a</div>
+    );
+  };
+
+  return (
+    <div className="raffle">
+      <div className="raffle__modal">
+        {renderStatus()}
+        <div>
+          <input
+            className="raffle__code"
+            type="phone"
+            value={code}
+            onChange={handleCodeChange}
+            placeholder="••"
+            autoFocus={true}
+          />
+        </div>
+        <button className="controls__item" type="button" onClick={handleCheck}>
+          Unlock
+        </button>
+      </div>
+    </div>
+  );
+};
